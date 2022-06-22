7a58d4eb821f52b7c6b8a3e2dd280549dd0c0149
Cynthia-ann Telfair <ctelfair20@gmail.com>
2022-06-22 14:46:01 -0500

    creates nav bar with styling

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,12 @@
+import '../NavBar/NavBar.css';
+
+function NavBar() {
+  return (
+    <div class="nav-bar">
+      <a class="home" href="/home">Home</a>
+      <a class="login" href="/login">Login</a>
+    </div>
+  )
+}
+
+export default NavBar;
