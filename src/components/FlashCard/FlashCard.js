12f17019b86cd7288a887c3f67e391116aba3001
Cynthia-ann Telfair <ctelfair20@gmail.com>
2022-07-22 18:01:23 -0500

    adds comment

--- src/components/FlashCard/FlashCard.js
+++ src/components/FlashCard/FlashCard.js
@@ -2,6 +2,7 @@ import './FlashCard.css';
 
 const FlashCard = ({ questionObj }) => {
 
+  // add click event to flip card to show answer
   return (
     <div id="flashcard-container">
       <span>
