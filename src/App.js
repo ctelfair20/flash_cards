5d26b627efdfa936a630bdbb04f49f655cf713e9
Cynthia-ann Telfair <ctelfair20@gmail.com>
2022-07-22 15:43:09 -0500

    adds qraphQL and flashcard component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 // import logo from './logo.svg';
 import './App.css';
-import NavBar from './components/NavBar/NavBar.js';
+import NavBar from './components/NavBar/NavBar';
+import FlashCard from "./components/FlashCard/FlashCard.js";
 
 function App() {
   return (
-    <NavBar />
+    <>
+      <NavBar />
+      <FlashCard />
+    </>
   );
 }
 
---
+++ src/components/FlashCard/FlashCard.js
@@ -0,0 +1,9 @@
+import rawData from '../../rawData';
+
+const FlashCard = () => {
+  return (
+    <div>hi</div>
+  );
+}
+
+export default FlashCard;
