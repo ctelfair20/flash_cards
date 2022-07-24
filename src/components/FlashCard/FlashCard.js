419d17cd76eeac36d3a2a600cbd3a72554db7109
Cynthia-ann Telfair <ctelfair20@gmail.com>
2022-07-23 21:58:08 -0500

    adds isFlipped state to flashcard component

--- src/components/FlashCard/FlashCard.js
+++ src/components/FlashCard/FlashCard.js
@@ -1,7 +1,8 @@
+import { useState } from 'react';
 import './FlashCard.css';
 
 const FlashCard = ({ questionObj }) => {
-
+  const [isFlipped, setFlipped] = useState(false);
   // add click event to flip card to show answer
   return (
     <div id="flashcard-container">
